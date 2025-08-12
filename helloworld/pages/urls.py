c2cc3c8206e580fac738daa0eb89e60a7a0b5e70
Mariana Valderrama Castañeda <mvaderrc1@eafit.edu.co>
2025-08-11 19:29:14 -0500

    image storage

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .utils import ImageLocalStorage
+from .views import HomePageView, AboutPageView, ContactPageView, ImageViewFactory, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
@@ -7,5 +8,10 @@ urlpatterns = [
     path('contact/', ContactPageView.as_view(), name='contact'),
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
-    path('products/<str:id>', ProductShowView.as_view(), name='show')
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'), 
 ]
